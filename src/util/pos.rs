33b59719515d2f44268184846f799c63a450981f
Albin Eriksson <albin-eriksson@outlook.com>
2022-12-17 20:21:50 +0100

    Add Display trait for Pos2d

--- src/util/pos.rs
+++ src/util/pos.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 #[derive(PartialEq, Eq, Clone, Copy, Default)]
 pub struct Pos2d<T> {
     pub x: T,
@@ -7,5 +9,11 @@ pub struct Pos2d<T> {
 impl<T> Pos2d<T> {
     pub fn new(x: T, y: T) -> Pos2d<T> {
         Pos2d { x, y }
+    }
+}
+
+impl<T: Display> Display for Pos2d<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
     }
 }
