2c5ae2fb027f35e16bf45dae687ded9749ca346e
Albin Eriksson <albin-eriksson@outlook.com>
2022-12-18 12:01:13 +0100

    Create Pos3d and derive Hash for Pos2d and Pos3d

--- src/util/pos.rs
+++ src/util/pos.rs
@@ -1,6 +1,6 @@
 use std::fmt::Display;
 
-#[derive(PartialEq, Eq, Clone, Copy, Default)]
+#[derive(PartialEq, Eq, Clone, Copy, Default, Hash)]
 pub struct Pos2d<T> {
     pub x: T,
     pub y: T
@@ -15,5 +15,24 @@ impl<T> Pos2d<T> {
 impl<T: Display> Display for Pos2d<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "({}, {})", self.x, self.y)
+    }
+}
+
+#[derive(PartialEq, Eq, Clone, Copy, Default, Hash)]
+pub struct Pos3d<T> {
+    pub x: T,
+    pub y: T,
+    pub z: T
+}
+
+impl<T> Pos3d<T> {
+    pub fn new(x: T, y: T, z: T) -> Pos3d<T> {
+        Pos3d { x, y, z }
+    }
+}
+
+impl<T: Display> Display for Pos3d<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "({}, {}, {})", self.x, self.y, self.z)
     }
 }
